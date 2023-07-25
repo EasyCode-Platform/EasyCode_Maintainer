766e668dd9031793047b7ecaf9d454f57913075f
Schrdingerscat <2524303785@qq.com>
2023-07-25 08:16:24 -0700

    a runnable demo

--- src/main/java/com/example/demo/controller/PodController.java
+++ src/main/java/com/example/demo/controller/PodController.java
@@ -1,6 +1,6 @@
 package com.example.demo.controller;
 
-
+import com.google.gson.GsonBuilder;
 import com.example.demo.model.vo.ResultVO;
 import io.kubernetes.client.openapi.ApiClient;
 import io.kubernetes.client.openapi.Configuration;
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.FileReader;
 
@@ -25,11 +26,12 @@ DELETE（DELETE）：从服务器删除资源。
 
 @RestController
 @RequestMapping("/pod")
-public class PodController{
+@Slf4j
+public class PodController {
 
     @GetMapping("/")
-    public ResultVO getAllPods() throws Exception{
-        String kubeConfigPath = "./resources/config";
+    public ResultVO getAllPods() throws Exception {
+        String kubeConfigPath = "./src/main/resources/config";
         ApiClient client = ClientBuilder
                 .kubeconfig(KubeConfig.loadKubeConfig(new FileReader(kubeConfigPath)))
                 .build();
@@ -38,8 +40,31 @@ public class PodController{
 
         // 调用客户端API取得所有pod信息
         V1PodList v1PodList = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
+        return ResultVO.ok(new GsonBuilder().setPrettyPrinting().create().toJson(v1PodList));
 
-        return ResultVO.ok(v1PodList);
+    }
+
+    @RequestMapping(value = "/hello")
+    public V1PodList hello() throws Exception {
+        // 存放K8S的config文件的全路径
+        String kubeConfigPath = "./src/main/resources/config";
+
+        // 以config作为入参创建的client对象，可以访问到K8S的API Server
+        ApiClient client = ClientBuilder
+                .kubeconfig(KubeConfig.loadKubeConfig(new FileReader(kubeConfigPath)))
+                .build();
+
+        Configuration.setDefaultApiClient(client);
+
+        CoreV1Api api = new CoreV1Api();
+
+        // 调用客户端API取得所有pod信息
+        V1PodList v1PodList = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
+
+        // 使用jackson将集合对象序列化成JSON，在日志中打印出来
+        log.info("pod info \n{}", new GsonBuilder().setPrettyPrinting().create().toJson(v1PodList));
+
+        return v1PodList;
     }
 
 }
