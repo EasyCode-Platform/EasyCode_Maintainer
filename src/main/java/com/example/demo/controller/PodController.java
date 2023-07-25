40533362a1be5cdc35e57b84380e0308e763bde6
梁永杰 <2524303785@qq.com>
2023-07-25 22:19:43 +0800

    add k8s api demo

--- src/main/java/com/example/demo/controller/PodController.java
+++ src/main/java/com/example/demo/controller/PodController.java
@@ -1,1 +1,45 @@
+package com.example.demo.controller;
 
+
+import com.example.demo.model.vo.ResultVO;
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.Configuration;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.V1PodList;
+import io.kubernetes.client.util.ClientBuilder;
+import io.kubernetes.client.util.KubeConfig;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.FileReader;
+
+/*
+GET（SELECT）：从服务器取出资源（一项或多项）。
+POST（CREATE）：在服务器新建一个资源。
+PUT（UPDATE）：在服务器更新资源（客户端提供完整资源数据）。
+PATCH（UPDATE）：在服务器更新资源（客户端提供需要修改的资源数据）。
+DELETE（DELETE）：从服务器删除资源。
+ */
+
+@RestController
+@RequestMapping("/pod")
+public class PodController{
+
+    @GetMapping("/")
+    public ResultVO getAllPods() throws Exception{
+        String kubeConfigPath = "./resources/config";
+        ApiClient client = ClientBuilder
+                .kubeconfig(KubeConfig.loadKubeConfig(new FileReader(kubeConfigPath)))
+                .build();
+        Configuration.setDefaultApiClient(client);
+        CoreV1Api api = new CoreV1Api();
+
+        // 调用客户端API取得所有pod信息
+        V1PodList v1PodList = api.listPodForAllNamespaces(null, null, null, null, null, null, null, null, null);
+
+        return ResultVO.ok(v1PodList);
+    }
+
+}
