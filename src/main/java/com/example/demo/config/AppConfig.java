4582aa49ff09cf3230b9c31a2a05719babc1d91e
梁永杰 <2524303785@qq.com>
2023-07-27 10:49:19 +0800

    add nodeController

---
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -0,0 +1,22 @@
+package com.example.demo.config;
+
+import io.kubernetes.client.openapi.ApiClient;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.util.ClientBuilder;
+import io.kubernetes.client.util.KubeConfig;
+import org.springframework.context.annotation.Bean;
+
+import java.io.FileReader;
+
+@org.springframework.context.annotation.Configuration
+public class AppConfig {
+    @Bean
+    public CoreV1Api coreV1Api() throws Exception {
+        String kubeConfigPath = "./src/main/resources/config";
+        ApiClient client = ClientBuilder
+                .kubeconfig(KubeConfig.loadKubeConfig(new FileReader(kubeConfigPath)))
+                .build();
+        io.kubernetes.client.openapi.Configuration.setDefaultApiClient(client);
+        return new CoreV1Api();
+    }
+}
